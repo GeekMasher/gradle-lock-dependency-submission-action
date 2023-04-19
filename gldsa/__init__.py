b6c2a962485a865260ee7a6c5162f10b83de3bb9
Mathew Payne <geekmasher@gmail.com>
2023-04-19 15:02:29 +0100

    feat: Add title to action

--- gldsa/__init__.py
+++ gldsa/__init__.py
@@ -1,3 +1,7 @@
 __name__ = "gldsa"
+__title__ = "gradle-lock-dependency-submission-action"
+
 __version__ = "0.1.0"
+
 __url__ = "https://github.com/GeekMasher/gradle-lock-dependency-submission-action"
+
